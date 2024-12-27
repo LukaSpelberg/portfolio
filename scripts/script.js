d3960dd314961de2a51f0a816c6208e7f8371a38
LukaSpelberg <luka.spelberg@hva.nl>
2024-12-27 19:06:24 +0100

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', () => {
-    const preloader = document.querySelector('.preLoader');
-    const mainContent = document.querySelector('header footer main');
-    const video = document.querySelector('.preLoader video');
+    const preloader = document.querySelector('.preLoader')
+    const mainContent = document.querySelector('header footer main')
+    const video = document.querySelector('.preLoader video')
 
 
     // Detect if the user is on a mobile device
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
     if (isMobile) {
-        video.src = '/videos/loaderMobiel.webm'; 
+        video.src = '../videos/loaderMobiel.webm'
     }
 
     setTimeout(() => {
-      preloader.style.display = 'none';
-      mainContent.style.display = 'block';
-    }, 5000); 
-});
+      preloader.style.display = 'none'
+      mainContent.style.display = 'block'
+    }, 5000)
+})
