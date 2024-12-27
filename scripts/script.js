e07f04584df1762066f6293bb6c07851cbc04f99
LukaSpelberg <luka.spelberg@hva.nl>
2024-12-27 22:34:17 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', () => {
     const preloader = document.querySelector('.preLoader')
     const mainContent = document.querySelector('header footer main')
-    const video = document.querySelector('.preLoader video')
+    const video = document.querySelector('video')
 
 
     // Detect if the user is on a mobile device
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
     if (isMobile) {
-        video.src = '../videos/loaderMobiel.webm'
+      video.src = '../videos/loaderMobiel.webm'
     }
 
     setTimeout(() => {
