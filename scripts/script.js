cdcaa5aa4a84038f30821868d3b08415b774eb8e
LukaSpelberg <luka.spelberg@hva.nl>
2024-12-27 13:27:48 +0100

    h

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,8 @@
     const preloader = document.querySelector('.preLoader');
     const mainContent = document.querySelector('video');
 
-    // Remove the preloader after 5 seconds or when the video ends
     setTimeout(() => {
       preloader.style.display = 'none';
       mainContent.style.display = 'block';
-    }, 5000); // Adjust timing based on your animation length
+    }, 5000); 
   });
