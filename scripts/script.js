890b0d70b30a653073ed663f89eb97a2e2b6f2ba
LukaSpelberg <luka.spelberg@hva.nl>
2025-01-05 14:13:34 +0100

    h

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,35 @@ window.addEventListener('load', () => {
     const mainContent = document.querySelectorAll('header, footer, main');
     const video = document.querySelector('video');
 
-    // Detect if the user is on a mobile device
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    // Check if the video source is 'videos/loaderFinal.webm'
+    if (video.src.includes('videos/loaderFinal.webm')) {
+        // Add background and display classes after 2000 milliseconds
+        setTimeout(() => {
+            document.body.classList.add('background');
+            mainContent.forEach(element => element.classList.add('display'));
+        }, 2000);
 
-    if (isMobile) {
-        video.src = 'https://lukaspelberg.github.io/portfolio/videos/loaderMobiel.webm';
+        // Hide preloader after 5000 milliseconds
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 5000);
+    } else {
+        // Detect if the user is on a mobile device
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+        if (isMobile) {
+            video.src = 'https://lukaspelberg.github.io/portfolio/videos/loader2final.mp4';
+        }
+
+        // Add background and display classes after 2000 milliseconds
+        setTimeout(() => {
+            document.body.classList.add('background');
+            mainContent.forEach(element => element.classList.add('display'));
+        }, 2000);
+
+        // Hide preloader after 5000 milliseconds
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 5000);
     }
-
-    // Add background and display classes after 2000 milliseconds
-    setTimeout(() => {
-        document.body.classList.add('background');
-        mainContent.forEach(element => element.classList.add('display'));
-    }, 2000);
-
-    // Hide preloader after 5000 milliseconds
-    setTimeout(() => {
-        preloader.style.display = 'none';
-    }, 5000);
 })
